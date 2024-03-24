bd6366c8f04e014758eff99e9ddfba61383f9177
sevenccn <153497100+sevenccn@users.noreply.github.com>
2024-03-24 18:33:48 +0100

    Add files via upload

--- inakai.js
+++ inakai.js
@@ -1,17 +1,46 @@
 const shirtinakai = document.querySelector("#shirtinakai")
 const jacketinakai = document.querySelector("#jacketinakai")
 const armorinakai = document.querySelector("#armorinakai")
+const end = document.querySelector("#end");
+// https://forum.freecodecamp.org/t/playing-local-mp3-file-in-visual-studio-code/451363/3 - audio//
+const muziekje = new Audio('awaken-136824.mp3');
 
-shirtinakai.addEventListener("click",()=>{
-    console.log("hello")
+let bovenkleding = document.querySelector("#bovenkleding")
+let GeluidAan = true
+
+//docent hulp//
+shirtinakai.addEventListener("click", () => {
     bovenkleding.src = "shirt_i.jpg"
 })
 
-
-jacketinakai.addEventListener("click",()=>{
+jacketinakai.addEventListener("click", () => {
     bovenkleding.src = "jacket_i.jpg"
 })
 
-armorinakai.addEventListener("click",()=>{
+armorinakai.addEventListener("click", () => {
     bovenkleding.src = "armor_i.jpg"
 })
+
+// mijn opdracht lamp/ douwe hulp om de knop op aan en uit te kunnen zetten//
+function muziek() {
+
+    if (GeluidAan == true) {
+        console.log("uit");
+        muziekje.pause();
+        GeluidAan = false
+
+
+    } else {
+        console.log("aan")
+        muziekje.play();
+        GeluidAan = true
+    }
+
+}
+
+end.addEventListener("click", muziek)
+
+//CHAT-GPT//
+function glitch() {
+    document.querySelector('.glitch-button').classList.toggle('glitch');
+}
--- javascript.js
+++ javascript.js
@@ -1,21 +1,17 @@
-console.log("hello")
-
-
-
 const shirtkenaji = document.querySelector("#shirtkenaji");
 const jacketkenaji = document.querySelector("#jacketkenaji");
 const armorkenaji = document.querySelector("#armorkenaji");
 const end = document.querySelector("#end");
-
 // https://forum.freecodecamp.org/t/playing-local-mp3-file-in-visual-studio-code/451363/3 - audio//
 const muziekje = new Audio('awaken-136824.mp3');
+
 let bovenkleding = document.querySelector("#bovenkleding")
+let GeluidAan = true
 
 //docent hulp//
 shirtkenaji.addEventListener("click", () => {
     bovenkleding.src = "shirt_k.jpg"
 })
-
 
 jacketkenaji.addEventListener("click", () => {
     bovenkleding.src = "jacket_k.jpg"
@@ -25,18 +21,24 @@ armorkenaji.addEventListener("click", () => {
     bovenkleding.src = "armor_k.jpg"
 })
 
-end.addEventListener("click", () => {
+// mijn opdracht lamp/ douwe hulp om de knop op aan en uit te kunnen zetten//
+function muziek() {
 
-muziekje.play();
-
-})
-
-
-
-
+    if (GeluidAan == true) {
+        console.log("uit");
+        muziekje.pause();
+        GeluidAan = false
 
 
+    } else {
+        console.log("aan")
+        muziekje.play();
+        GeluidAan = true
+    }
 
+}
+
+end.addEventListener("click", muziek)
 
 //CHAT-GPT//
 function glitch() {
